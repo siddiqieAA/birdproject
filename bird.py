c907ba522789f42b49eebe02839abd461f346dca
siddiqieAA <115996329+siddiqieAA@users.noreply.github.com>
2022-10-18 20:43:52 +0800

    Add files via upload

---
+++ bird.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.core.pylabtools import figsize
+
+import folium
+from folium.plugins import HeatMap
+import plotly.express as px
+import seaborn as sns
+
+## Import LabelEncoder from sklearn
+from sklearn.preprocessing import LabelEncoder
+#Calculate by using PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import plot_confusion_matrix
+
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import r2_score
+
+data=pd.read_csv('bird.csv')
+
+data
+
+dcopy=data.copy()
+
+dcopy
+
+dcopy.shape
+
+dcopy.columns
+
+dcopy.dtypes
+
+
+
+
+
+
